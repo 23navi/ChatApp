d62f0083a1bc19b4c51a2a1f4207bfe725f06daa
23navi <navisureka23@gmail.com>
2025-05-05 00:57:40 +0530

    register for upstash redis

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,1 +1,16 @@
-console.log("Hello World")
+import dotenv from "dotenv"
+dotenv.config() // Reads from .env by default
+
+
+
+const PORT = parseInt(process.env.PORT || "3001")
+const HOST = process.env.HOST || "0.0.0.0" //Setting it to 0.0.0.0 for docker as it can't resolve localhost
+const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://locahost:3000"
+const REDIS_ENDPOINT = process.env.REDIS_ENDPOINT
+
+if (!REDIS_ENDPOINT) {
+    console.error("REDIS_ENDPOINT not found")
+    process.exit(1)
+}
+
+
