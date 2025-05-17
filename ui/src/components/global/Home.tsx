88bdf5f4b1bcec3b28c8c43c5fd2c5f6e3e31b2a
23navi <navisureka23@gmail.com>
2025-05-17 09:23:21 +0530

    add basic message input form

--- ui/src/components/global/Home.tsx
+++ ui/src/components/global/Home.tsx
@@ -1,5 +1,7 @@
 import { useSocket } from "@/hooks/useSocket";
-import { useEffect } from "react";
+import { FormEvent, useEffect, useState } from "react";
+import { Textarea } from "../ui/textarea";
+import { Button } from "../ui/button";
 
 // Socket channels
 // const NEW_MESSAGE_CHANNEL = "chat:new-message";
@@ -7,6 +9,8 @@ import { useEffect } from "react";
 
 export default function Home() {
   const socket = useSocket();
+
+  const [newMessage, setNewMessage] = useState("");
 
   useEffect(() => {
     if (!socket) return;
@@ -30,17 +34,30 @@ export default function Home() {
     };
   }, [socket]);
 
-  return <h1>Hello</h1>;
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    console.log(newMessage);
+
+    setNewMessage("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="flex items-center">
+      <Textarea
+        className="rounded-lg mr-4"
+        placeholder="Tell is what's on your mind"
+        value={newMessage}
+        onChange={(e) => setNewMessage(e.target.value)}
+        maxLength={255}
+      />
+
+      <Button className="h-full">Send message</Button>
+    </form>
+  );
 }
-
-
-
-
-
-
 
 // In the above useEffect, we have 3 different dependency management
 
-// 1) useEffect(() => { ... }, [socket])   - Best: run the effect when the component mounts, and any time the socket value changes. 
+// 1) useEffect(() => { ... }, [socket])   - Best: run the effect when the component mounts, and any time the socket value changes.
 // 2) useEffect(() => { ... }, [])    - Won't work if the socket was null on first render (run the effect only once when the component mounts — and never again, regardless of changes to socket)
 // 3) useEffect(() => { ... }) -  Run the effect after every render, including initial render and every subsequent re-render.
