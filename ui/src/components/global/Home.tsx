16b71177ea4cc131dc830cd79f69149f8ea3e74b
23navi <navisureka23@gmail.com>
2025-05-17 09:40:13 +0530

    add simple message emitter

--- ui/src/components/global/Home.tsx
+++ ui/src/components/global/Home.tsx
@@ -4,13 +4,14 @@ import { Textarea } from "../ui/textarea";
 import { Button } from "../ui/button";
 
 // Socket channels
-// const NEW_MESSAGE_CHANNEL = "chat:new-message";
+const NEW_MESSAGE_CHANNEL = "chat:new-message";
 // const CONNECTION_COUNT_UPDATED_CHANNEL = "chat:connection-count-updated";
 
 export default function Home() {
   const socket = useSocket();
 
   const [newMessage, setNewMessage] = useState("");
+  const [connectionCount] = useState(0);
 
   useEffect(() => {
     if (!socket) return;
@@ -38,21 +39,31 @@ export default function Home() {
     e.preventDefault();
     console.log(newMessage);
 
+    // When we are here, we are not sure if the socket is connected or not, we need to handle this in a better way, we can have a state which keeps a track of socket connection and disable the input button and also show a connecting ui loader.
+    socket?.emit(NEW_MESSAGE_CHANNEL, {
+      message: newMessage,
+    });
+
     setNewMessage("");
   }
 
   return (
-    <form onSubmit={handleSubmit} className="flex items-center">
-      <Textarea
-        className="rounded-lg mr-4"
-        placeholder="Tell is what's on your mind"
-        value={newMessage}
-        onChange={(e) => setNewMessage(e.target.value)}
-        maxLength={255}
-      />
+    <main className="flex flex-col p-4 w-full max-w-3xl m-auto">
+      <h1 className="text-4xl font-bold text-center mb-4">
+        Chat ({connectionCount})
+      </h1>
+      <form onSubmit={handleSubmit} className="flex items-center">
+        <Textarea
+          className="rounded-lg mr-4"
+          placeholder="Tell is what's on your mind"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          maxLength={255}
+        />
 
-      <Button className="h-full">Send message</Button>
-    </form>
+        <Button className="h-full">Send message</Button>
+      </form>
+    </main>
   );
 }
 
@@ -61,3 +72,9 @@ export default function Home() {
 // 1) useEffect(() => { ... }, [socket])   - Best: run the effect when the component mounts, and any time the socket value changes.
 // 2) useEffect(() => { ... }, [])    - Won't work if the socket was null on first render (run the effect only once when the component mounts — and never again, regardless of changes to socket)
 // 3) useEffect(() => { ... }) -  Run the effect after every render, including initial render and every subsequent re-render.
+
+
+
+// Note: we have two disconnect with socket socket.disconnect() and socket.on("disconnect")
+// socket.disconnect() is a method to disconnect from the react side
+// socket.on("disconnect") is an event listener which react which there is disconnect on the client side.
