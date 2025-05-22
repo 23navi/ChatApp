8d56df11cdf207846fc83e59c7551dac7dd49037
23navi <navisureka23@gmail.com>
2025-05-22 21:56:53 +0530

    update env and caddy

--- ui/src/hooks/useSocket.tsx
+++ ui/src/hooks/useSocket.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import io, { Socket } from "socket.io-client";
 
+const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || "ws://127.0.0.1:3001";
+
 export const useSocket = () => {
   const [socket, setSocket] = useState<Socket | null>(null);
 
   useEffect(() => {
     // connect to socket server
-    const socketIO = io("ws://127.0.0.1:3001", {
+    const socketIO = io(SOCKET_URL, {
       withCredentials: true,
       reconnection: true,
       upgrade: true,
